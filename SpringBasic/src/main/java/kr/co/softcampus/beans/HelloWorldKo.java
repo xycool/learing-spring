df305aa1cb6ddd33c7fda6c2cf6fbf201b6c7262
Ethan Ji <jchwche@gmail.com>
2022-02-20 20:30:30 +0900

    .

---
+++ SpringBasic/src/main/java/kr/co/softcampus/beans/HelloWorldKo.java
@@ -0,0 +1,9 @@
+package kr.co.softcampus.beans;
+
+public class HelloWorldKo implements HelloWorld{
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요");
+	}
+}
