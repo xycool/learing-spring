199acc4469b596453279d0ed0556b8994af29696
Ethan Ji <jchwche@gmail.com>
2022-02-20 20:26:26 +0900

    .

---
+++ SpringBasic/src/main/java/kr/co/softcampus/beans/HelloWorldEn.java
@@ -0,0 +1,11 @@
+package kr.co.softcampus.beans;
+
+public class HelloWorldEn implements HelloWorld {
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("HI~~");
+	}
+
+}
--- SpringBasic/src/main/java/kr/co/softcampus/main/MainClass.java
+++ SpringBasic/src/main/java/kr/co/softcampus/main/MainClass.java
@@ -1,10 +1,28 @@
 package kr.co.softcampus.main;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import kr.co.softcampus.beans.HelloWorld;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//
+		// beans.xml 파일을 로딩한다.
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/co/softcampus/config/beans.xml");
+		
+		// xml에 정의한 bean 객체의 주소값을 가져온다.
+		HelloWorld hello1 = (HelloWorld)ctx.getBean("hello");
+		callMethod(hello1);
+		
+		HelloWorld hello2 = ctx.getBean("hello", HelloWorld.class);
+		callMethod(hello2);
+		
+		ctx.close();
+	}
+	
+	public static void callMethod(HelloWorld hello) {
+		hello.sayHello();
 	}
 
 }
